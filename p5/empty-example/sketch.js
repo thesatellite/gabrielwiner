d30818313361ffe366dc684c9dad14de37980382
Gabriel Winer <gabrielwiner@gmail.com>
2018-05-14 22:15:46 -0400

    Update sketch.js

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   createCanvas(710, 400);
-  background(250);
+  background(102);
 }
 
 function draw() {
-  stroke(20);
+  stroke(255);
   if (mouseIsPressed === true) {
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
