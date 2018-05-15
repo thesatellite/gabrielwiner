dd981704f5a2f68e0fb219dbf60cae80c83d4ff3
Gabriel Winer <gabrielwiner@gmail.com>
2018-05-14 23:44:53 -0400

    Update sketch.js

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,10 +1,42 @@
+var ptouchIsDown;
+var pmouseIsPressed;
 function setup() {
- 	createCanvas(windowWidth, windowHeight);
-	strokeWeight(10)
-	stroke(0);
+  // uncomment this line to make the canvas the full size of the window
+  createCanvas(windowWidth, windowHeight);
+  strokeWeight(5);
+  stroke(0);
+  fill(0);
+  background(255);
+  ptouchIsDown = touchIsDown;
+  pmouseIsPressed = mouseIsPressed;
 }
 
-function touchMoved() {
-	line(mouseX, mouseY, pmouseX, pmouseY);
-	return false;
+function draw() {
+  if (ptouchIsDown && touchIsDown){
+    stroke(255, 0, 0);
+    line(touchX, touchY, ptouchX, ptouchY);
+  }
+  if (pmouseIsPressed && mouseIsPressed){
+    stroke(0, 0, 255);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+  ptouchIsDown = touchIsDown;
+  pmouseIsPressed = mouseIsPressed;
+}
+
+function touchMoved(){
+  //stroke(255, 0, 0);
+  //line(touchX, touchY, ptouchX, ptouchY);
+  return false;
+}
+
+function touchEnded(){
+  //stroke(0, 0, 255);
+  //line(touchX, touchY, ptouchX, ptouchY);
+  return false;
+}
+
+function mouseDragged(){
+  //stroke(0, 255, 0);
+  //line(mouseX, mouseY, pmouseX, pmouseY);
 }
